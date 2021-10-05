df60a4408e275a6bdc967c4c31131a1bf0f45a49
Korey Andrus <korey86@gmail.com>
2021-10-05 14:54:51 -0500

    display charts and fetch countries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, formControl, FormControl } from '@material-ui/core';
+import { StylesContext } from '@material-ui/styles';
+
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
 
 const CountryPicker = () => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
+    console.log(fetchedCountries);
     return (
-        <h1>
-           CountryPicker 
-        </h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+            </NativeSelect>
+        </FormControl>
     )
 }
 
